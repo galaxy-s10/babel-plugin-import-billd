56008bd1b3c6e28cc68b6b8a18ab9d49d588c406
shuisheng <2274751790@qq.com>
2022-11-20 00:54:43 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,46 @@
+# 源码
+
+[https://github.com/galaxy-s10/babel-plugin-import-billd](https://github.com/galaxy-s10/babel-plugin-import-billd)
+
+# 概况
+
+参考了[babel-plugin-import](https://github.com/umijs/babel-plugin-import)，实现了我认为核心的三个配置项：`libraryName`、`libraryDirectory`、`style`
+
+# 测试
+
+由于现代的组件库大多数都实现了原生的 ES modules 的 tree shaking，因此我们需要下载旧版的组件库才能测试出效果，这里我使用了 ant-design-vue 的 1.1.0 版本以及 antd 的 2.13.14 版本进行测试。
+
+当遇到下面的代码时，会仅仅将 Button 和 Switch 进行打包：
+
+```js
+import { Button, Switch } from 'ant-design-vue';
+console.log(Button, Switch);
+```
+
+# 使用
+
+babel.config.js：
+
+```js
+plugins: [
+  // ...
+  [
+    'import-billd',
+    // Options在 babel@7+ 中不能是数组，但是可以添加带名字的插件来支持多个依赖。
+    { libraryName: 'ant-design-vue', libraryDirectory: 'lib', style: 'css' },
+    'aaa', // 这个名字可以随便起
+  ],
+  [
+    'import-billd',
+    { libraryName: 'antd', libraryDirectory: 'lib', style: 'css' },
+    'bbb',
+  ],
+],
+```
+
+# 引用
+
+- [@babel/helper-module-imports](https://babel.dev/docs/en/babel-helper-module-imports)
+- [babel-plugin-import](https://github.com/umijs/babel-plugin-import)
+- [babel-handbook](https://github.com/jamiebuilds/babel-handbook/blob/master/translations/zh-Hans/plugin-handbook.md)
+- [https://astexplorer.net](https://astexplorer.net)
---
+++ src/main.js
@@ -0,0 +1,84 @@
+const { addDefault, addSideEffect } = require('@babel/helper-module-imports');
+
+export default function () {
+  // console.log(arguments)
+  const pathList = {};
+
+  return {
+    // 每个文件都会走一遍visitor
+    visitor: {
+      /**
+       * visitor的Program里的enter可以理解为，会在visitor里的所有操作执行前，执行一次enter；
+       * 而exit可以理解为，在visitor里的所有操作执行完了后，会执行一次exit
+       */
+      Program: {
+        enter(path, state) {
+          console.log('enter', state.filename);
+        },
+        exit(path, state) {
+          console.log('exit', state.filename);
+          Object.keys(pathList).forEach((key) => {
+            !pathList[key].removed && pathList[key].remove();
+          });
+        },
+      },
+
+      /**
+       * 引入模块的时候会执行这个函数，如import { bbb } from 'aaa'
+       */
+      ImportDeclaration(path, state) {
+        const { node } = path;
+        if (!node) return;
+        const { value } = node.source; // 这个value是aaa
+        const { libraryName } = state.opts;
+        if (value === libraryName) {
+          node.specifiers.forEach((spec) => {
+            pathList[spec.local.name] = path;
+          });
+        }
+      },
+
+      /**
+       * 调用表达式的时候会执行这个函数，如：console.log(),这里调用了console.log这个方法
+       */
+      CallExpression(path, state) {
+        // state.opts:能获取babel.config.js里传进来的参数：
+        // state.cwd:node进程工作目录？
+        // state.file:当前匹配的文件
+        // state.filename:当前匹配的文件路径，如：'/Users/huangshuisheng/Desktop/hss/github/webpack-multi-static/src/page/index.ts'
+        const { node } = path;
+        const file =
+          (path && path.hub && path.hub.file) || (state && state.file);
+        const { libraryName, libraryDirectory, style = true } = state.opts;
+        node.arguments = node.arguments.map((arg) => {
+          const { name: argName } = arg;
+          let res = {};
+          Object.keys(pathList).forEach((item) => {
+            if (item === argName) {
+              res = addDefault(
+                file.path,
+                `${libraryName}/${libraryDirectory}/${item}`,
+                { nameHint: item }
+              );
+              if (style === true) {
+                addSideEffect(
+                  file.path,
+                  `${libraryName}/${libraryDirectory}/${item}/style/index.js`
+                );
+              } else if (style === 'css') {
+                addSideEffect(
+                  file.path,
+                  `${libraryName}/${libraryDirectory}/${item}/style/css.js`
+                );
+              }
+            }
+          });
+          if (Object.keys(res).length) {
+            return res;
+          }
+          return arg;
+        });
+      },
+    },
+  };
+}
